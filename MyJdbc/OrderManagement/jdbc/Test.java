67fd81d62b49783e5650ccd74926bb59b8a1a3e0
poppoppuppylove <140991571+poppoppuppylove@users.noreply.github.com>
2023-12-30 19:11:58 +0800

    Update Test.java

--- MyJdbc/OrderManagement/jdbc/Test.java
+++ MyJdbc/OrderManagement/jdbc/Test.java
@@ -6,19 +6,25 @@ import java.util.Date;
 public class Test {
 
     public static void main(String[] args) throws SQLException {
-        // 添加商品
-        ProductDao.addProduct(8, "新商品", 50);
+        // 添加商品成功
+        ProductDao.addProduct(8, "新商品2", 88);
 
         // 查询所有商品
         System.out.println("所有商品:");
         ProductDao.getAllProducts();
+
+        // 添加商品失败
+        ProductDao.addProduct(8, "新商品2", 88);
 
         // 按商品价格升序排序
         System.out.println("\n按商品价格升序排序:");
         SortDao.sortProductsByPriceAsc();
 
         // 创建订单
-        OrderDao.createOrder(8, new Date(), 50);
+        OrderDao.createOrder(8, new Date(), 88);
+        OrderDao.createOrder(2, new Date(), 6);
+        OrderDao.createOrder(88, new Date(), 20);
+        OrderDao.createOrder(3, new Date(), 20);
 
         // 查询所有订单
         System.out.println("\n所有订单:");
@@ -28,25 +34,31 @@ public class Test {
         System.out.println("\n按订单时间降序排序");
         SortDao.sortOrdersByDateDesc();
 
-        // 测试按订单价格升序排序
+        // 按订单价格升序排序
         System.out.println("\n按订单价格升序排序:");
         SortDao.sortOrdersByPriceAsc();
 
-        // 删除商品及相关订单
+        // 删除商品,若有订单还要删相关订单
         System.out.println("\n删除商品及相关订单:");
+        ProductDao.deleteProductAndOrders(2);
         ProductDao.deleteProductAndOrders(8);
+        ProductDao.getAllProducts();
 
-        // 测试删除订单
+        // 添加商品
+        ProductDao.addProduct(2, "剪刀", 6);
+        ProductDao.addProduct(5, "咖啡", 28);
+
+        // 删除订单
         System.out.println("\n删除订单后还剩:");
         OrderDao.deleteOrder(8);
         OrderDao.getAllOrders();
 
-        // 测试更新商品信息
+        // 更新商品信息
         System.out.println("\n更新商品信息:");
         ProductDao.updateProduct(6, "管他什么", 999);
         ProductDao.getAllProducts();
 
-        // 测试根据商品ID查询商品信息
+        // 根据商品ID查询商品信息
         System.out.println("\n根据商品ID查询商品信息:");
         ProductDao.getProductById(4);
     }
